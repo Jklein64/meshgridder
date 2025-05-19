c044dffb549e95eabfeb5989522e64f2e30a5b4e
Jason Klein <jklein.64@verizon.net>
2025-05-18 22:13:19 -0400

    Add infrastructure for profiling

---
+++ test/profile_meshgridder.py
@@ -0,0 +1,30 @@
+"""
+Computes cell areas for a random mesh to simulate a typical workload.
+"""
+
+import cProfile
+from argparse import ArgumentParser
+from sys import argv
+
+import mitsuba as mi
+from util import random_mi_mesh
+
+from meshgridder.numpy import compute_cell_areas
+
+mi.set_variant("llvm_ad_rgb")
+
+
+def main(grid_rows, grid_cols, outfile):
+    mesh = random_mi_mesh()
+    with cProfile.Profile() as profiler:
+        compute_cell_areas(mesh, grid_rows, grid_cols)
+        profiler.dump_stats(outfile)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Runs an example load for profiling.")
+    parser.add_argument("--rows", dest="rows", type=int)
+    parser.add_argument("--cols", dest="cols", type=int)
+    parser.add_argument("-o", "--outfile", dest="outfile", type=str)
+    args = parser.parse_args(argv[1:])
+    main(args.rows, args.cols, args.outfile)
--- test/test_meshgridder.py
+++ test/test_meshgridder.py
@@ -2,13 +2,17 @@
 Property-based tests for the mesh gridder.
 """
 
-import drjit as dr
 import mitsuba as mi
 import numpy as np
 from pytest import approx
-from scipy.spatial import Delaunay
+from util import random_mi_mesh
 
-from meshgridder import BoundingBox, Grid, compute_cell_areas, polygon_area
+from meshgridder.numpy import (
+    BoundingBox,
+    Grid,
+    compute_cell_areas,
+    polygon_area,
+)
 
 mi.set_variant("llvm_ad_rgb")
 
@@ -50,69 +54,5 @@ def test_correct_cell_area_sum():
     for tri_vertices in vertices[faces]:
         true_surface_area += polygon_area(tri_vertices, dim="3d")
 
-    assert total_surface_area == approx(true_surface_area)
-
-
-def random_mi_mesh(size=(6, 7), grid_size=(8, 12), offset=(1, 2)):
-    # number of cells
-    grid_size_x, grid_size_y = grid_size
-    # total width of all cells
-    grid_width, grid_height = size
-    # offset from origin
-    grid_offset_x, grid_offset_y = offset
-    # individual width of one cell
-    cell_width = grid_width / grid_size_x
-    cell_height = grid_height / grid_size_y
-
-    # stratified sampling in xy plane
-    vertices_x = np.linspace(
-        grid_offset_x + 0.5 * cell_width,
-        grid_offset_x + grid_width + 0.5 * cell_width,
-        grid_size_x,
-        endpoint=False,
-    )
-    vertices_y = np.linspace(
-        grid_offset_y + 0.5 * cell_height,
-        grid_offset_y + grid_height + 0.5 * cell_height,
-        grid_size_y,
-        endpoint=False,
-    )
-    vertices_xy = np.stack(np.meshgrid(vertices_x, vertices_y), axis=-1)
-    jitters = (np.random.rand(*vertices_xy.shape) - 0.5) * np.array(
-        [cell_width, cell_height]
-    )
-
-    # create vertical offsets
-    vertices_z = np.random.rand(*vertices_xy.shape[:2])
-    vertices = np.stack(
-        [*(vertices_xy + jitters).transpose(2, 0, 1), vertices_z], axis=-1
-    )
-    vertices = np.reshape(vertices, (grid_size_x * grid_size_y, 3))
-
-    # use delaunay triangulation to create connectivity
-    faces = Delaunay(vertices[..., 0:2]).simplices
-    # create texture coordinates
-    bbox = BoundingBox.from_points(vertices[..., 0:2])
-    texcoords = np.copy(vertices[..., 0:2])
-    texcoords -= np.array([bbox.x_min, bbox.y_min])
-    texcoords /= np.array([bbox.width, bbox.height])
-    # flip y axis so texcoord (0, 0) is in top left
-    texcoords[..., 1] = 1 - texcoords[..., 1]
-
-    mi_mesh = mi.Mesh(
-        "mesh",
-        vertex_count=vertices.shape[0],
-        face_count=faces.shape[0],
-        has_vertex_texcoords=True,
-    )
-
-    params = mi.traverse(mi_mesh)
-    # new drjit constructors expect 2/3 as first dim instead of last
-    vertices_dr = mi.Point3f(vertices.T)
-    texcoords_dr = mi.Point2f(texcoords.T)
-    faces_dr = mi.Point3u(faces.T)
-    params["vertex_positions"] = mi.Float(dr.ravel(vertices_dr))
-    params["vertex_texcoords"] = mi.Float(dr.ravel(texcoords_dr))
-    params["faces"] = dr.auto.UInt(dr.ravel(faces_dr))
-
-    return mi_mesh
+    # expect relative error of 1e-5 instead of 1e-6 due to the nudging
+    assert total_surface_area == approx(true_surface_area, rel=1e-5)
