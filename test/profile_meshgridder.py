b1f175abe57c7196ec5d93f6b76558b4600a3bb8
Jason Klein <jklein.64@verizon.net>
2025-05-21 17:50:17 -0400

    Add mesh grid size to profiling script

--- test/profile_meshgridder.py
+++ test/profile_meshgridder.py
@@ -5,6 +5,7 @@ Computes cell areas for a random mesh to simulate a typical workload.
 import cProfile
 from argparse import ArgumentParser
 from sys import argv
+from time import perf_counter
 
 import mitsuba as mi
 from util import random_mi_mesh
@@ -15,10 +16,15 @@ mi.set_variant("llvm_ad_rgb")
 
 
 def main(grid_rows, grid_cols, outfile):
-    mesh = random_mi_mesh()
+    mesh = random_mi_mesh(grid_size=(8, 6))
     with cProfile.Profile() as profiler:
+        start = perf_counter()
         compute_cell_areas(mesh, grid_rows, grid_cols)
+        stop = perf_counter()
         profiler.dump_stats(outfile)
+        print(
+            f"Took {stop - start} seconds for a {grid_rows}x{grid_cols} grid."
+        )
 
 
 if __name__ == "__main__":
