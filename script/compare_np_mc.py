b137a8c1dae0ec9a2e1c0f89e5ce132fd0fd1101
Jason Klein <jklein.64@verizon.net>
2025-05-25 15:54:01 -0400

    Print runtime

--- script/compare_np_mc.py
+++ script/compare_np_mc.py
@@ -3,6 +3,7 @@ Plot cell area results for comparison of np and mc methods.
 """
 
 import os
+from time import perf_counter
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -15,6 +16,8 @@ from meshgridder.np import compute_cell_areas as compute_cell_areas_np
 
 mi.set_variant("llvm_ad_rgb")
 
+samples_per_cell = 1024
+
 filename = "out/np_vs_mc.npz"
 if os.path.exists(filename):
     data = np.load(filename)
@@ -22,11 +25,25 @@ if os.path.exists(filename):
     areas_mc = data["mc"]
 else:
     # create a random mesh
+    rng = np.random.default_rng(seed=1)
+    np.random.seed(1)
     mi_mesh = random_mi_mesh()
+    start = perf_counter()
     areas_mc = compute_cell_areas(
-        mi_mesh, grid_rows=100, grid_cols=100, samples_per_cell=64
+        mi_mesh,
+        grid_rows=100,
+        grid_cols=100,
+        samples_per_cell=samples_per_cell,
+        rng=rng,
     )
+    stop = perf_counter()
+    print(f"time (mc): {stop - start} seconds")
+
+    start = perf_counter()
     areas_np = compute_cell_areas_np(mi_mesh, grid_rows=100, grid_cols=100)
+    stop = perf_counter()
+    print(f"time (np): {stop - start} seconds")
+
     np.savez(filename, mc=areas_mc, np=areas_np)
 
 # areas_mc[np.isnan(areas_mc)] = 0
@@ -55,10 +72,9 @@ axs[0].yaxis.set_inverted(True)
 axs[0].set_title("np method")
 axs[1].pcolormesh(areas_mc, norm=norm)
 axs[1].yaxis.set_inverted(True)
-axs[1].set_title("mc method (64 samples/cell)")
+axs[1].set_title(f"mc method ({samples_per_cell} samples/cell)")
 tmp = axs[2].pcolormesh(np.abs(areas_np - areas_mc))
 axs[2].yaxis.set_inverted(True)
 axs[2].set_title("absolute error")
 plt.colorbar(tmp)
-# plt.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap="viridis"), ax=axs)
 plt.show()
