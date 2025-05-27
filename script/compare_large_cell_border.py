7997cd1924ea9c2608eb3601b64be3745eaaab44
Jason Klein <jklein.64@verizon.net>
2025-05-27 17:07:33 -0400

    Plot absolute errors

--- script/compare_large_cell_border.py
+++ script/compare_large_cell_border.py
@@ -10,7 +10,7 @@ import mitsuba as mi
 import numpy as np
 from common import random_mi_mesh
 from scipy.spatial import ConvexHull
-from scipy.stats import ecdf
+from scipy.stats import ecdf, gaussian_kde
 from tqdm import tqdm
 
 from meshgridder.mc import compute_cell_areas as compute_cell_areas_mc
@@ -49,18 +49,29 @@ def main(rows, cols):
     fig.tight_layout()
 
     fig = plt.figure(2)
-    ax = fig.add_subplot()
-    mappable = ax.imshow(
+    axs = [fig.add_subplot(1, 2, k) for k in range(1, 3)]
+    # relative error
+    mappable = axs[0].imshow(
         np.abs(cell_areas_sh - cell_areas_mc) / np.abs(cell_areas_sh),
         extent=extents,
     )
-    plot_mesh(texcoords, faces, ax=ax, color="red", alpha=0.2)
-    ax.set_title("Monte Carlo relative error")
-    plt.colorbar(mappable)
+    plot_mesh(texcoords, faces, ax=axs[0], color="red", alpha=0.2)
+    axs[0].set_title("Monte Carlo relative error")
+    plt.colorbar(mappable, ax=axs[0])
+    # absolute error
+    mappable = axs[1].imshow(
+        np.abs(cell_areas_sh - cell_areas_mc), extent=extents
+    )
+    plot_mesh(texcoords, faces, ax=axs[1], color="red", alpha=0.2)
+    axs[1].set_title("Monte Carlo absolute error")
+    plt.colorbar(mappable, ax=axs[1])
+    w, h = fig.get_size_inches()
+    fig.set_size_inches(2 * w, 0.95 * h)
+    fig.savefig("out/rel_abs_err.png")
 
     fig = plt.figure(3)
     axs = [fig.add_subplot(1, 2, k) for k in [1, 2]]
-    ns = np.logspace(np.log10(50_000), np.log10(10_000_000), 20)
+    ns = np.logspace(np.log10(50_000), np.log10(1_000_000), 20)
     times = []
     errors = []
     for i, n in tqdm(enumerate(ns), total=len(ns)):
@@ -71,10 +82,12 @@ def main(rows, cols):
         cell_areas = compute_cell_areas_mc(mesh, rows, cols, int(n), rng=rng)
         stop = perf_counter()
         times.append(stop - start)
-        errors.append(np.abs(cell_areas_sh - cell_areas) / cell_areas_sh)
+        # errors.append(np.abs(cell_areas_sh - cell_areas) / cell_areas_sh)
+        errors.append(np.abs(cell_areas_sh - cell_areas))
     colors = mpl.cm.viridis(np.linspace(0, 1, len(ns)))
     for i in range(len(ns)):
-        res = ecdf(np.ravel(errors[i]))
+        err_flat = np.ravel(errors[i])
+        res = ecdf(err_flat[np.isfinite(err_flat)])
         res.cdf.plot(color=colors[i], ax=axs[0])
         res.cdf.plot(color=colors[i], ax=axs[1])
     axs[0].set_title("Relative error CDFs")
@@ -88,12 +101,56 @@ def main(rows, cols):
     cbar.ax.ticklabel_format(style="plain", useOffset=False)
     cbar.set_label("sample count")
 
-    fig = plt.figure(4)
+    # use kde to estimate pdfs
+    fig = plt.figure(4, constrained_layout=True)
+    axs = [fig.add_subplot(2, 2, k) for k in range(1, 5)]
+    colors = mpl.cm.viridis(np.linspace(0, 1, len(ns)))
+    xs = np.logspace(np.log10(1e-8), np.log10(10), 1000)
+    for i, err in enumerate(errors):
+        err_flat = np.ravel(err)
+        # fit kde to log data
+        data = np.log10(err_flat)
+        kde = gaussian_kde(data[np.isfinite(data)])
+        axs[2].plot(xs, kde(np.log10(xs)), color=colors[i])
+        axs[3].plot(xs, kde(np.log10(xs)), color=colors[i])
+        res = ecdf(err_flat[np.isfinite(err_flat)])
+        res.sf.plot(color=colors[i], ax=axs[0])
+        res.sf.plot(color=colors[i], ax=axs[1])
+    axs[0].set_title("Absolute error eCDF survival functions")
+    # axs[0].set(xlim=[0, 0.3], xlabel="relative error")
+    axs[0].set(xlim=[0, 0.1], xlabel="absolute error")
+    axs[2].set_title("Absolute error PDFs (estimated with KDE)")
+    axs[2].set(xlim=[0, 0.1], xlabel="absolute error")
+    # axs[2].set(xlim=[0, 0.3], xlabel="relative error")
+    axs[1].set_title("Absolute error eCDF survival functions")
+    # axs[1].set(xlim=[1e-6, 1], xlabel="relative error (log)")
+    axs[1].set(xlim=[1e-8, 1], xlabel="absolute error (log)")
+    axs[1].set_xscale("log")
+    axs[3].set_title("Absolute error PDFs (estimated with KDE)")
+    axs[3].set(xlim=[1e-8, 1], xlabel="absolute error (log)")
+    axs[3].set_xscale("log")
+    plt.subplots_adjust(hspace=0.4)
+    w, h = fig.get_size_inches()
+    fig.set_size_inches(2 * w, 1.5 * h)
+    # https://gist.github.com/jakevdp/8a992f606899ac24b711
+    cmap = mpl.cm.get_cmap("viridis", len(ns))
+    norm = mpl.colors.Normalize(0, len(ns))
+    formatter = plt.FuncFormatter(lambda v, _: int(ns[v]))
+    cbar = plt.colorbar(
+        mpl.cm.ScalarMappable(norm, cmap=cmap),
+        ticks=np.arange(len(ns)),
+        format=formatter,
+        ax=axs,
+    )
+    cbar.set_ticks(np.arange(len(ns)) + 0.5, labels=ns.astype(int))
+    cbar.set_label("sample count")
+    fig.savefig("out/cdfs_abs.png")
+
+    fig = plt.figure(5)
     norm = mpl.colors.Normalize(np.min(errors), np.max(errors))
     hull = ConvexHull(texcoords)
     for i, err in enumerate(errors):
         ax = fig.add_subplot(4, 5, i + 1)
-        # TODO this norm isn't working right now
         ax.imshow(err, extent=extents, norm=norm)
         ax.set_title(f"n = {int(ns[i])}")
         for line in texcoords[hull.simplices]:
@@ -122,4 +179,4 @@ def plot_mesh(vertices, faces, ax=None, **line_kwargs):
 
 if __name__ == "__main__":
     # test with a small grid
-    main(rows=12, cols=8)
+    main(rows=20, cols=30)
