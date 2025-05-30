4b3f5a0d17ca632599bb0bf6491bcc8a3f04cfd3
Jason Klein <jklein.64@verizon.net>
2025-05-30 19:57:53 -0400

    Use coordinate frames in dr method

--- src/meshgridder/dr.py
+++ src/meshgridder/dr.py
@@ -15,37 +15,31 @@ def compute_cell_areas(
     samples=1_000_000,
     return_mesh=False,
 ):
-    spp = int(samples / (rows * cols))
-
     mesh_params = mi.traverse(mesh)
-    vertices = dr.reshape(Array3f, mesh_params["vertex_positions"], (3, -1))
+    vert_global = dr.reshape(Array3f, mesh_params["vertex_positions"], (3, -1))
     faces = dr.reshape(Array3u, mesh_params["faces"], (3, -1))
-    # create basis vectors whose span is the projection plane
-    basis_u, basis_v = mi.coordinate_system(proj_normal)
     # project and normalize vertices to [0, 1]
-    vertices_u = dr.dot(vertices, basis_u)
-    vertices_v = dr.dot(vertices, basis_v)
-    bbox_min = Array2f(dr.min(vertices_u), dr.min(vertices_v))
-    bbox_max = Array2f(dr.max(vertices_u), dr.max(vertices_v))
+    proj_frame = mi.Frame3f(proj_normal)
+    vert_local = proj_frame.to_local(vert_global)
+    bbox_min = Array2f(dr.min(vert_local.x), dr.min(vert_local.y))
+    bbox_max = Array2f(dr.max(vert_local.x), dr.max(vert_local.y))
     bbox_extents = Array2f(bbox_max.x - bbox_min.x, bbox_max.y - bbox_min.y)
-    texcoords = Array2f(
-        (vertices_u - bbox_min.x) / bbox_extents.x,
-        (vertices_v - bbox_min.y) / bbox_extents.y,
-    )
+    texcoords = (vert_local.xy - bbox_min) / bbox_extents
 
     # create new mesh with the computed texcoords
     texcoord_mesh = mi.Mesh(
         "texcoord_mesh",
-        vertex_count=dr.width(vertices),
+        vertex_count=dr.width(vert_global),
         face_count=dr.width(faces),
         has_vertex_texcoords=True,
     )
     params = mi.traverse(texcoord_mesh)
-    params["vertex_positions"] = dr.ravel(vertices)
+    params["vertex_positions"] = dr.ravel(vert_global)
     params["vertex_texcoords"] = dr.ravel(texcoords)
     params["faces"] = dr.ravel(faces)
     params.update()
 
+    spp = int(samples / (rows * cols))
     # generate spp samples per grid cell
     center_u, center_v = dr.meshgrid(
         (dr.arange(Float, cols) + 0.5) / cols,
