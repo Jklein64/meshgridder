2ee4a639b75d36841945c1b8f8921d74e00c1c47
Jason Klein <jklein.64@verizon.net>
2025-05-30 19:46:48 -0400

    Add test for rotated meshes and projection plane normal vectors

--- test/test_dr.py
+++ test/test_dr.py
@@ -2,6 +2,7 @@
 Tests for the dr method, which is similar to mc.
 """
 
+import drjit as dr
 import mitsuba as mi
 import numpy as np
 from common import compute_mesh_area, random_mi_mesh
@@ -28,4 +29,48 @@ def test_correct_area_sum():
     mesh_area_ref = compute_mesh_area(mesh)
 
     # this relative error is not great
+    assert mesh_area == approx(mesh_area_ref, rel=1e-3)
+
+
+def test_correct_when_rotated_mesh():
+    # create random mesh and rotate its vertices
+    mesh = random_mi_mesh()
+    mesh_params = mi.traverse(mesh)
+    vertices = dr.reshape(mi.Point3f, mesh_params["vertex_positions"], (3, -1))
+    rotation = mi.Transform4f().rotate(axis=[0, 1, 0], angle=30)
+    new_vertices = rotation @ vertices
+    proj_normal = rotation @ mi.Point3f(0, 0, 1)
+    mesh_params["vertex_positions"] = dr.ravel(new_vertices)
+    mesh_params.update()
+
+    mesh_area_ref = compute_mesh_area(mesh)
+
+    # using a flat projection plane should fail
+    try:
+        cell_areas = compute_cell_areas(
+            mesh,
+            rows=150,
+            cols=200,
+            samples=10_000_000,
+        ).numpy()
+        mesh_area = np.sum(cell_areas)
+
+        # this assertion should fail
+        assert mesh_area == approx(mesh_area_ref, rel=1e-3)
+        # it didn't fail, so the test failed
+        assert False
+
+    except AssertionError:
+        assert True
+
+    # but using the correct projection plane should succeed
+    cell_areas = compute_cell_areas(
+        mesh,
+        rows=150,
+        cols=200,
+        proj_normal=proj_normal,
+        samples=10_000_000,
+    ).numpy()
+    mesh_area = np.sum(cell_areas)
+
     assert mesh_area == approx(mesh_area_ref, rel=1e-3)
